054a143c53555159792aa2090a684058619e629a
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2025-09-24 13:48:04 +0200

    full path for rm

--- csf/uninstall.sh
+++ csf/uninstall.sh
@@ -11,8 +11,8 @@ then
     systemctl stop csf.service
     systemctl stop lfd.service
 
-    rm -fv /usr/lib/systemd/system/csf.service
-    rm -fv /usr/lib/systemd/system/lfd.service
+    /bin/rm -fv /usr/lib/systemd/system/csf.service
+    /bin/rm -fv /usr/lib/systemd/system/lfd.service
     systemctl daemon-reload
 else
     if [ -f /etc/redhat-release ]; then
@@ -27,20 +27,20 @@ else
         rc-update del lfd default
         rc-update del csf default
     elif [ -f /etc/slackware-version ]; then
-        rm -vf /etc/rc.d/rc3.d/S80csf
-        rm -vf /etc/rc.d/rc4.d/S80csf
-        rm -vf /etc/rc.d/rc5.d/S80csf
-        rm -vf /etc/rc.d/rc3.d/S85lfd
-        rm -vf /etc/rc.d/rc4.d/S85lfd
-        rm -vf /etc/rc.d/rc5.d/S85lfd
+        /bin/rm -vf /etc/rc.d/rc3.d/S80csf
+        /bin/rm -vf /etc/rc.d/rc4.d/S80csf
+        /bin/rm -vf /etc/rc.d/rc5.d/S80csf
+        /bin/rm -vf /etc/rc.d/rc3.d/S85lfd
+        /bin/rm -vf /etc/rc.d/rc4.d/S85lfd
+        /bin/rm -vf /etc/rc.d/rc5.d/S85lfd
     else
         /sbin/chkconfig csf off
         /sbin/chkconfig lfd off
         /sbin/chkconfig csf --del
         /sbin/chkconfig lfd --del
     fi
-    rm -fv /etc/init.d/csf
-    rm -fv /etc/init.d/lfd
+    /bin/rm -fv /etc/init.d/csf
+    /bin/rm -fv /etc/init.d/lfd
 fi
 
 if [ -e "/usr/local/cpanel/bin/unregister_appconfig" ]; then
@@ -48,14 +48,14 @@ if [ -e "/usr/local/cpanel/bin/unregister_appconfig" ]; then
 	/usr/local/cpanel/bin/unregister_appconfig csf
 fi
 
-rm -fv /etc/chkserv.d/lfd
-rm -fv /usr/sbin/csf
-rm -fv /usr/sbin/lfd
-rm -fv /etc/cron.d/csf_update
-rm -fv /etc/cron.d/lfd-cron
-rm -fv /etc/cron.d/csf-cron
-rm -fv /etc/logrotate.d/lfd
-rm -fv /usr/local/man/man1/csf.man.1
+/bin/rm -fv /etc/chkserv.d/lfd
+/bin/rm -fv /usr/sbin/csf
+/bin/rm -fv /usr/sbin/lfd
+/bin/rm -fv /etc/cron.d/csf_update
+/bin/rm -fv /etc/cron.d/lfd-cron
+/bin/rm -fv /etc/cron.d/csf-cron
+/bin/rm -fv /etc/logrotate.d/lfd
+/bin/rm -fv /usr/local/man/man1/csf.man.1
 
 /bin/rm -fv /usr/local/cpanel/whostmgr/docroot/cgi/addon_csf.cgi
 /bin/rm -Rfv /usr/local/cpanel/whostmgr/docroot/cgi/csf
@@ -67,11 +67,11 @@ rm -fv /usr/local/man/man1/csf.man.1
 /bin/rm -Rfv /usr/local/cpanel/Cpanel/Config/ConfigObj/Driver/ConfigServercsf
 /bin/touch /usr/local/cpanel/Cpanel/Config/ConfigObj/Driver
 
-rm -fv /var/run/chkservd/lfd
+/bin/rm -fv /var/run/chkservd/lfd
 sed -i 's/lfd:1//' /etc/chkserv.d/chkservd.conf
 /scripts/restartsrv_chkservd
 
-rm -Rfv /etc/csf /usr/local/csf /var/lib/csf
+/bin/rm -Rfv /etc/csf /usr/local/csf /var/lib/csf
 
 echo
 echo "...Done"
