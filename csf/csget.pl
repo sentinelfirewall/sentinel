8d47280cc37f0310ae5de185a21dfc6010f93ce5
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2025-09-26 01:24:13 +0200

    Download version From Github directly, just csf

--- csf/csget.pl
+++ csf/csget.pl
@@ -17,78 +17,77 @@
 # You should have received a copy of the GNU General Public License along with
 # this program; if not, see <https://www.gnu.org/licenses>.
 ###############################################################################
+
 use strict;
 use warnings;
 use diagnostics;
 
+# Daemonize
 if (my $pid = fork) {
-	exit 0;
+    exit 0;
 } elsif (defined($pid)) {
-	$pid = $$;
+    $pid = $$;
 } else {
-	die "Error: Unable to fork: $!";
+    die "Error: Unable to fork: $!";
 }
 chdir("/");
-close (STDIN);
-close (STDOUT);
-close (STDERR);
-open STDIN, "<","/dev/null";
-open STDOUT, ">","/dev/null";
-open STDERR, ">","/dev/null";
+close(STDIN);
+close(STDOUT);
+close(STDERR);
+open STDIN,  "<", "/dev/null";
+open STDOUT, ">", "/dev/null";
+open STDERR, ">", "/dev/null";
 
 $0 = "ConfigServer Version Check";
 
-my @downloadservers = ("https://raw.githubusercontent.com/stefanpejcic/sentinelfw/refs/heads/main/");
+# Single download server
+my $downloadserver = "https://raw.githubusercontent.com/stefanpejcic/sentinelfw/refs/heads/main/";
 
+# Single version file to check
+my $version_file = "/csf/version.txt";
+my $local_file   = "/var/lib/configserver/csf.txt";
+
+# Prepare local directory
 system("mkdir -p /var/lib/configserver/");
-system("rm -f /var/lib/configserver/*.txt /var/lib/configserver/*error");
+unlink $local_file;
+unlink $local_file . ".error" if -e $local_file . ".error";
 
+# Determine download command
 my $cmd;
-if (-e "/usr/bin/curl") {$cmd = "/usr/bin/curl -skLf -m 120 -o"}
-elsif (-e "/usr/bin/wget") {$cmd = "/usr/bin/wget -q -T 120 -O"}
-else {
-	open (my $ERROR, ">", "/var/lib/configserver/error");
-	print $ERROR "Cannot find /usr/bin/curl or /usr/bin/wget to retrieve product versions\n";
-	close ($ERROR);
-	exit;
-}
-my $GET;
-if (-e "/usr/bin/GET") {$GET = "/usr/bin/GET -sd -t 120"}
-
-my %versions;
-if (-e "/etc/csf/csf.pl") {$versions{"/csf/version.txt"} = "/var/lib/configserver/csf.txt"}
-
-if (scalar(keys %versions) == 0) {
-	unlink $0;
-	exit;
+if (-e "/usr/bin/curl") {
+    $cmd = "/usr/bin/curl -skLf -m 120 -o";
+} elsif (-e "/usr/bin/wget") {
+    $cmd = "/usr/bin/wget -q -T 120 -O";
+} else {
+    open(my $ERROR, ">", "/var/lib/configserver/error");
+    print $ERROR "Cannot find /usr/bin/curl or /usr/bin/wget to retrieve product versions\n";
+    close($ERROR);
+    exit;
 }
 
+# Optional GET command
+my $GET = (-e "/usr/bin/GET") ? "/usr/bin/GET -sd -t 120" : "";
+
+# Random sleep unless --nosleep
 unless ($ARGV[0] eq "--nosleep") {
-	system("sleep",int(rand(60 * 60 * 6)));
-}
-for (my $x = @downloadservers; --$x;) {
-		my $y = int(rand($x+1));
-		if ($x == $y) {next}
-		@downloadservers[$x,$y] = @downloadservers[$y,$x];
+    system("sleep", int(rand(60 * 60 * 6)));
 }
 
-foreach my $server (@downloadservers) {
-	foreach my $version (keys %versions) {
-		unless (-e $versions{$version}) {
-			if (-e $versions{$version}.".error") {unlink $versions{$version}.".error"}
-			my $status = system("$cmd $versions{$version} $server$version");
-			if ($status) {
-				if ($GET ne "") {
-					open (my $ERROR, ">", $versions{$version}.".error");
-					print $ERROR "$server$version - ";
-					close ($ERROR);
-					my $GETstatus = system("$GET $server$version >> $versions{$version}".".error");
-				} else {
-					open (my $ERROR, ">", $versions{$version}.".error");
-					print $ERROR "Failed to retrieve latest version from Github";
-					close ($ERROR);
-				}
-			}
-		}
-	}
+# Fetch version
+unless (-e $local_file) {
+    unlink $local_file . ".error" if -e $local_file . ".error";
+
+    my $status = system("$cmd $local_file $downloadserver$version_file");
+    if ($status) {
+        if ($GET ne "") {
+            open(my $ERROR, ">", $local_file . ".error");
+            print $ERROR "$downloadserver$version_file - ";
+            close($ERROR);
+            system("$GET $downloadserver$version_file >> $local_file.error");
+        } else {
+            open(my $ERROR, ">", $local_file . ".error");
+            print $ERROR "Failed to retrieve latest version from Github\n";
+            close($ERROR);
+        }
+    }
 }
